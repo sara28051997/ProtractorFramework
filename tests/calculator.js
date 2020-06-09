21c5de014501d838b4b02af68a1fd39518f3f6c3
SarathKumar Nithyananthan <sarathkumarnithyananthan@hotmail.com>
2020-06-09 17:40:07 +0530

    My third commit

--- tests/calculator.js
+++ tests/calculator.js
@@ -3,13 +3,27 @@ describe("demo calculator tests", function () {
   it("addition test", function () {
     homepage.get("https://juliemr.github.io/protractor-demo/");
 
-    homepage.enterFirst("2");
+    homepage.enterFirst("4");
 
-    homepage.enterSecond("4");
+    homepage.enterSecond("2");
 
     homepage.clickgo();
 
     homepage.verify("6");
+
+    browser.sleep(3000);
+  });
+
+  it("subtraction test", function () {
+    homepage.get("https://juliemr.github.io/protractor-demo/");
+
+    homepage.enterFirst("4");
+
+    homepage.enterSecond("2");
+
+    homepage.clickgo();
+
+    homepage.verify("1");
 
     browser.sleep(3000);
   });
